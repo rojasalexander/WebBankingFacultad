cc50a27a49bbcce125e7778fa43707298e754aa6
FabriRejala <fa.rejala03@gmail.com>
2022-11-15 14:46:37 -0300

    connectProgram

--- src/main/java/com/taobanking/connectProgram.java
+++ src/main/java/com/taobanking/connectProgram.java
@@ -6,7 +6,6 @@ import java.sql.*;
 public class connectProgram 
 {
     public static void main(String[] args) {
-        // for ip 34.151.212.162 and port 5432
         String jcbURL = "jdbc:postgresql://34.151.212.162:5432/postgres";
         String username = "postgres";
         String password = "password";
