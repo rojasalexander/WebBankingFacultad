ca0f017f3aa2524848acc7df17c49b3ecc5ef0a7
FabriRejala <fa.rejala03@gmail.com>
2022-11-15 15:24:57 -0300

    interfaces and database

---
+++ src/main/java/com/taobanking/database/AccountDB.java
@@ -0,0 +1,8 @@
+package com.taobanking.database;
+
+import com.taobanking.interfaces.Account;
+
+public class AccountDB extends DataBaseConnection {
+    private Account account = new Account();
+
+}
---
+++ src/main/java/com/taobanking/interfaces/Account.java
@@ -0,0 +1,12 @@
+package com.taobanking.interfaces;
+
+public class Account {
+    private Integer account_id;
+    private Integer account_number;
+    private Integer account_pin;
+    private Integer account_transaction_pin;
+    private Integer account_balance;
+    private Integer client_id;
+    private Integer bank_id;
+
+}
