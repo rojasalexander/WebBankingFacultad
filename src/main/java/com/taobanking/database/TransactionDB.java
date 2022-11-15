c5c2872d0e31c5fb47d9dcb4feac8651539eca27
enrisal2000 <enrisal2000@fpuna.edu.py>
2022-11-15 18:30:23 -0300

    Update of TransactionDB

--- src/main/java/com/taobanking/database/TransactionDB.java
+++ src/main/java/com/taobanking/database/TransactionDB.java
@@ -1,41 +1,59 @@
 package com.taobanking.database;
 
+import java.sql.SQLException;
+
 import com.taobanking.classes.Transaction;
 
-public class TransactionDB {
+public class TransactionDB extends DataBaseConnection {
     Transaction transaction;
 
+    /**
+     * Constructor
+     * @param transaction
+     */
     public TransactionDB(Transaction transaction) {
         this.transaction = transaction;
     }
 
+     /**
+      * Insertar una nueva transaccion en la tabla
+      * @param transaction
+      * @throws SQLException
+      */
     public void insertTransaction() {
         try {
-            DataBaseConnection database = new DataBaseConnection();
-            database.getConnection();
+            this.getConnection();
 
             String query = "INSERT INTO transactions (transaction_date, transaction_amount, account_id, transaction_number" +
             "VALUES ('" + this.transaction.getTransactionDate() + "', " + this.transaction.getTransactionAmount() + ", " +
             this.transaction.getAccountId() + ", " + this.transaction.getTransactionNumber() + ");";
 
-            database.executeQuery(query);
-            database.closeConnection();
+            this.executeQuery(query);
+            this.closeConnection();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error connecting to PostgreSQL database");
         }
     }
-
+    
+    /**
+     * Actualizar una transaccion en la tabla
+     * @param transaction
+     * @throws SQLException
+     */
     public void updateTransaction() {
         try {
-            DataBaseConnection database = new DataBaseConnection();
-            database.getConnection();
+            this.getConnection();
 
             String query = "UPDATE transactions SET transaction_date = '" + this.transaction.getTransactionDate() + "', " +
             "transaction_amount = " + this.transaction.getTransactionAmount() +
             " WHERE transaction_id = " + this.transaction.getTransactionId() + ";";
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            this.executeQuery(query);
+            this.closeConnection();
+
+        } catch (SQLException e) {
+            System.out.println("Error connecting to PostgreSQL database");
         }
     }
 }
